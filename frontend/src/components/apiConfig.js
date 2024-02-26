7623173c3ba1572f50c019fd85c341ac30765cc4
IhorMar <rmg1203@gmail.com>
2024-02-26 19:22:22 +0200

    created new app and made changes in code

--- frontend/src/components/apiConfig.js
+++ frontend/src/components/apiConfig.js
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
+const API_BASE_URL = 'http://127.0.0.1:8000/';
 const API_RANDOM_USER_URL = 'https://random-data-api.com/api/v2/users';
 const API_RANDOM_BANK_URL = 'https://random-data-api.com/api/v2/banks';
 
